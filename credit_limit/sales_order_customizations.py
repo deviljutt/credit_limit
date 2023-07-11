dfcc0e09160dcc17ad0eda7f3916992a8722f146
Nabeel Goraya <itsgoraya@gmail.com>
2023-07-11 14:43:42 +0500

    update

--- credit_limit/sales_order_customizations.py
+++ credit_limit/sales_order_customizations.py
@@ -34,47 +34,48 @@ def sales_order_on_submit(doc, method):
     price_level_three = int(docz.price_level_three)
 
 
-    xx = credit_limit-ordertotal;
-    exists = None;
-    if xx < 0:
-        xx = abs(xx)
-        if xx <= price_level_one:
-            approval_role = "Level 1"
-            csv_values = om_profile
-            value_array = csv_values.split(",")
-            value_to_check = user
-            if value_to_check in value_array:
-                exists = "approve"
+    if credit_limit is not None:
+        xx = credit_limit-ordertotal;
+        exists = None;
+        if xx < 0:
+            xx = abs(xx)
+            if xx <= price_level_one:
+                approval_role = "Level 1"
+                csv_values = om_profile
+                value_array = csv_values.split(",")
+                value_to_check = user
+                if value_to_check in value_array:
+                    exists = "approve"
+                else:
+                    exists = "Only Level 1 can approve"
+            elif price_level_one < xx <= price_level_two:
+                approval_role = "Level 2"
+                csv_values = ar_profile
+                value_array = csv_values.split(",")
+                value_to_check = user
+                if value_to_check in value_array:
+                    exists = "approve"
+                else:
+                    exists = "Only Level 2 can approve"
             else:
-                exists = "Only Level 1 can approve"
-        elif price_level_one < xx <= price_level_two:
-            approval_role = "Level 2"
-            csv_values = ar_profile
-            value_array = csv_values.split(",")
-            value_to_check = user
-            if value_to_check in value_array:
-                exists = "approve"
-            else:
-                exists = "Only Level 2 can approve"
+                approval_role = "CEO"
+                csv_values = ar_vp
+                value_array = csv_values.split(",")
+                value_to_check = user
+                if value_to_check in value_array:
+                    exists = "approves"
+                else:
+                    exists = "Only CEO can approve"
+
         else:
-            approval_role = "CEO"
-            csv_values = ar_vp
-            value_array = csv_values.split(",")
-            value_to_check = user
-            if value_to_check in value_array:
-                exists = "approves"
-            else:
-                exists = "Only CEO can approve"
-
-    else:
-         pass   
+            pass   
 
 
-    if exists is not None and exists != 'approves':
-        converted_string = str(exists) 
-        throw(converted_string)
-    else:
-         pass 
+        if exists is not None and exists != 'approves':
+            converted_string = str(exists) 
+            throw(converted_string)
+        else:
+            pass 
           
 
   
