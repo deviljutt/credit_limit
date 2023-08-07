ee856b3f388b77e0656677eb3a3d31ff887a7e88
Nabeel Goraya <itsgoraya@gmail.com>
2023-08-07 12:05:40 +0500

    Update sales_order_customizations.py

--- credit_limit/sales_order_customizations.py
+++ credit_limit/sales_order_customizations.py
@@ -130,10 +130,10 @@ def sales_order_on_submit(doc, method):
     
     outstandingdays = get_date_difference_from_last_sale_invoice(customer_name);
 
-    credit_term_one = int(docz.credit_term_one)
-    credit_term_two = int(docz.credit_term_two)
-    credit_term_three = int(docz.credit_term_three)
-    credit_term_four = int(docz.credit_term_four)
+    credit_term_one = int(docz.credit_term_one)+credit_term
+    credit_term_two = int(docz.credit_term_two)+credit_term
+    credit_term_three = int(docz.credit_term_three)+credit_term
+    credit_term_four = int(docz.credit_term_four)+credit_term
 
     xx = int(credit_term) - int(outstandingdays) 
     xx = abs(xx)
