454496eacb7dbce89f27c21ad639304773563c88
Nabeel Goraya <itsgoraya@gmail.com>
2023-08-22 17:59:48 +0500

    Update my_custom_app.js

--- credit_limit/public/js/my_custom_app.js
+++ credit_limit/public/js/my_custom_app.js
@@ -1,9 +1,108 @@
-frappe.pages['my-custom-settings'] = {
-  title: __('My Custom Settings'),
-  parent: 'Setup',
-  icon: 'fa fa-cogs',
-  onload: function(wrapper) {
-    wrapper.page.set_title(__('My Custom Settings'));
-    // Add your custom settings HTML and logic here
+frappe.ui.form.on('Customer', {
+  shipping_supplier: function (frm) {
+      var link_value = frm.doc.shipping_supplier;
+      if (link_value) {
+        frappe.call({
+            method: 'frappe.client.get',
+            args: {
+                doctype: 'Supplier',
+                name: link_value
+            },
+            callback: function (response) {
+                if (response.message) {
+                    var linked_doc = response.message;
+                    firsvalue = linked_doc.supplier_names;
+                    frm.set_value('shipping_name', firsvalue);   
+                }
+            }
+        });
+    }
   }
-};
+});
+
+frappe.ui.form.on('Sales Order', {
+  customer: function (frm) {
+      var link_value = frm.doc.customer;
+      if (link_value) {
+        frappe.call({
+            method: 'frappe.client.get',
+            args: {
+                doctype: 'Customer',
+                name: link_value
+            },
+            callback: function (response) {
+                if (response.message) {
+                    var linked_doc = response.message;
+                    shipping_supplier = linked_doc.shipping_supplier;
+                    shipping_name = linked_doc.shipping_name;
+                    shipping_detail = linked_doc.shipping_detail;
+
+                    frm.set_value('shipping_supplier', shipping_supplier);   
+                    frm.set_value('shipping_names', shipping_name);  
+                    frm.set_value('shipping_phone', shipping_detail);  
+
+                }
+            }
+        });
+    }
+  }
+});
+
+
+frappe.ui.form.on('Sales Invoice', {
+  customer: function (frm) {
+      var link_value = frm.doc.customer;
+      if (link_value) {
+        frappe.call({
+            method: 'frappe.client.get',
+            args: {
+                doctype: 'Customer',
+                name: link_value
+            },
+            callback: function (response) {
+                if (response.message) {
+                    var linked_doc = response.message;
+                    shipping_supplier = linked_doc.shipping_supplier;
+                    shipping_name = linked_doc.shipping_name;
+                    shipping_detail = linked_doc.shipping_detail;
+
+                    frm.set_value('shipping_suppliers', shipping_supplier);   
+                    frm.set_value('shipping_names', shipping_name);  
+                    frm.set_value('shipping_detail', shipping_detail);  
+
+                }
+            }
+        });
+    }
+  }
+});
+
+
+
+frappe.ui.form.on('Delivery Note', {
+  customer: function (frm) {
+      var link_value = frm.doc.customer;
+      if (link_value) {
+        frappe.call({
+            method: 'frappe.client.get',
+            args: {
+                doctype: 'Customer',
+                name: link_value
+            },
+            callback: function (response) {
+                if (response.message) {
+                    var linked_doc = response.message;
+                    shipping_supplier = linked_doc.shipping_supplier;
+                    shipping_name = linked_doc.shipping_name;
+                    shipping_detail = linked_doc.shipping_detail;
+
+                    frm.set_value('shipping_suppliers', shipping_supplier);   
+                    frm.set_value('shipping_names', shipping_name);  
+                    frm.set_value('shipping_detail', shipping_detail);  
+
+                }
+            }
+        });
+    }
+  }
+});
