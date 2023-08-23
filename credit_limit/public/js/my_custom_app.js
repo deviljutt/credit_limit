403434329b44f0f6613f85a13462cc3325a7fdf2
Nabeel Goraya <itsgoraya@gmail.com>
2023-08-23 12:43:06 +0500

    Update my_custom_app.js

--- credit_limit/public/js/my_custom_app.js
+++ credit_limit/public/js/my_custom_app.js
@@ -25,3 +25,60 @@ frappe.ui.form.on('Customer', {
     }
   });
 
+
+frappe.ui.form.on('Sales Order', {
+  customer: function (frm) {
+      var link_value = frm.doc.customer;
+      if (link_value) {
+        frappe.call({
+            method: 'frappe.client.get',
+            args: {
+                doctype: 'Customer',
+                name: link_value
+            },
+            callback: function (response) {
+                if (response.message) {
+                    var linked_doc = response.message;
+                    shipping_supplier = linked_doc.custom_shipping_supplier;
+                    shipping_name = linked_doc.shipping_name;
+                    shipping_detail = linked_doc.shipping_detail;
+
+                    frm.set_value('shipping_suppliers', shipping_supplier);   
+                    frm.set_value('shipping_names', shipping_name);  
+                    frm.set_value('shipping_phone', shipping_detail);  
+
+                }
+            }
+        });
+    }
+  }
+});
+
+frappe.ui.form.on('Sales Invoice', {
+  customer: function (frm) {
+      var link_value = frm.doc.customer;
+      if (link_value) {
+        frappe.call({
+            method: 'frappe.client.get',
+            args: {
+                doctype: 'Customer',
+                name: link_value
+            },
+            callback: function (response) {
+                if (response.message) {
+                    var linked_doc = response.message;
+                    shipping_supplier = linked_doc.custom_shipping_supplier;
+                    shipping_name = linked_doc.shipping_name;
+                    shipping_detail = linked_doc.shipping_detail;
+
+                    frm.set_value('shipping_suppliers', shipping_supplier);   
+                    frm.set_value('shipping_names', shipping_name);  
+                    frm.set_value('shipping_phone', shipping_detail);  
+
+                }
+            }
+        });
+    }
+  }
+});
+
