5a04803309324b5fc0c2d1238711486733650863
Nabeel Goraya <itsgoraya@gmail.com>
2023-08-25 04:12:04 +0500

    Update my_custom_app.js

--- credit_limit/public/js/my_custom_app.js
+++ credit_limit/public/js/my_custom_app.js
@@ -128,3 +128,32 @@ frappe.ui.form.on('Packing Slip', {
       }
     }
   });
+
+
+frappe.ui.form.on('Sales Invoice', {
+    onload: function (frm) {
+          var link_value = frm.doc.customer;
+		  if (link_value) {
+			frappe.call({
+				method: 'frappe.client.get',
+				args: {
+					doctype: 'Customer',
+					name: link_value
+				},
+				callback: function (response) {
+					if (response.message) {
+						var linked_doc = response.message;
+						shipping_supplier = linked_doc.custom_shipping_supplier;
+						shipping_name = linked_doc.shipping_name;
+						shipping_detail = linked_doc.shipping_detail;
+
+						frm.set_value('shipping_suppliers', shipping_supplier);   
+						frm.set_value('shipping_names', shipping_name);  
+						frm.set_value('shipping_phone', shipping_detail);  
+
+					}
+				}
+			});
+    }
+	}
+  });
