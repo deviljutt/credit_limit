046bd12481102a9b9f7e5b26cec2854ba8fd9a45
Nabeel Goraya <itsgoraya@gmail.com>
2023-08-25 04:16:33 +0500

    Update hooks.py

--- credit_limit/hooks.py
+++ credit_limit/hooks.py
@@ -20,7 +20,7 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/credit_limit/css/credit_limit.css"
 # app_include_js = "/assets/credit_limit/js/credit_limit.js"
-app_include_js = "/assets/credit_limit/js/my_custom_app.js?"+"444"
+app_include_js = "/assets/credit_limit/js/my_custom_app.js?"+"441"
 
 
 
