aafa514397afff4f9892e4f8d28c5a6569c1d2c7
Nabeel Goraya <itsgoraya@gmail.com>
2023-09-05 10:09:38 +0500

    Update sales_order_customizations.py

--- credit_limit/sales_order_customizations.py
+++ credit_limit/sales_order_customizations.py
@@ -12,27 +12,27 @@ from datetime import datetime
 def sales_order_on_submit(doc, method):
     doctype = "Credit Limit Settings"
 
-    customer_name = doc.customer
-    customer = frappe.get_doc("Customer", doc.customer)
+    # customer_name = doc.customer
+    # customer = frappe.get_doc("Customer", doc.customer)
 
-    user = frappe.get_doc("User", frappe.session.user)
-    user = user.email
+    # user = frappe.get_doc("User", frappe.session.user)
+    # user = user.email
 
 
-    if customer.credit_limits:
-        credit_limit = customer.credit_limits[0].credit_limit
-    else:
-        credit_limit = 0
+    # if customer.credit_limits:
+    #     credit_limit = customer.credit_limits[0].credit_limit
+    # else:
+    #     credit_limit = 0
         
         
-    saleorders = frappe.db.get_list('Sales Order', filters={ 'customer': doc.customer,'status': ['in', ['To Deliver and Bill','To Deliver','Completed']] }, fields=['total']); 
-    total_amount = 0
-    for entry in saleorders:
-        total_amount += entry.get('total', 0)
+    # saleorders = frappe.db.get_list('Sales Order', filters={ 'customer': doc.customer,'status': ['in', ['To Deliver and Bill','To Deliver','Completed']] }, fields=['total']); 
+    # total_amount = 0
+    # for entry in saleorders:
+    #     total_amount += entry.get('total', 0)
     
 
-    ordertotal = doc.total    
-    credit_limit =  credit_limit - total_amount
+    # ordertotal = doc.total    
+    # credit_limit =  credit_limit - total_amount
     
     docz = frappe.get_doc(doctype, doctype)  
     
@@ -42,17 +42,72 @@ def sales_order_on_submit(doc, method):
     ceo_profile = docz.ceo_profile.split(",")   
 
 
-    price_level_one = docz.price_level_one
-    price_level_two = docz.price_level_two
-    price_level_three = docz.price_level_three
+    # price_level_one = docz.price_level_one
+    # price_level_two = docz.price_level_two
+    # price_level_three = docz.price_level_three
     
-    exists = None
-    role = None
+    # exists = None
+    # role = None
 
 
-    if credit_limit is not None: 
-        xx = credit_limit-ordertotal
+    # if credit_limit is not None: 
+    #     xx = credit_limit-ordertotal
 
+    #     if xx < 0:  
+    #         xx = abs(xx)  
+    #         if user in om_profile:
+    #             lower_bound, upper_bound = map(int, price_level_one.split('-'))
+    #             if lower_bound <= xx <= upper_bound:
+    #                 exists = "approve"
+    #                 role = "om Profile"
+    #         if user in ar_profile:
+    #             lower_bound, upper_bound = map(int, price_level_two.split('-'))
+    #             if lower_bound <= xx <= upper_bound:
+    #                 exists = "approve"
+    #                 role = "AR Profile"
+    #         if user in ar_vp:
+    #             lower_bound, upper_bound = map(int, price_level_three.split('-'))
+    #             if lower_bound <= xx <= upper_bound:
+    #                 exists = "approve"
+    #                 role = "AR VP"
+    #         if user in ceo_profile:
+    #             exists = "approve"
+    #             role = "ceo"
+                 
+    #     else:
+    #         exists = "approve"    
+        
+
+    # if exists != 'approve':
+    #     converted_string = f'Credit Limit difference is {xx}. Contact upper-level permissions.'
+    #     throw(converted_string)
+    # else:
+    #     pass
+    
+    customer_name = doc.customer
+    customer = frappe.get_doc("Customer", doc.customer)
+    user = frappe.get_doc("User", frappe.session.user)
+    user = user.email
+
+
+    posting_date = doc.transaction_date
+    date_object = datetime.strptime(posting_date, "%Y-%m-%d")
+    posting_date = datetime.timestamp(date_object)
+
+
+    current_datetime = datetime.now()
+    timestamp = datetime.timestamp(current_datetime)
+
+    time_difference =  timestamp - posting_date
+    days = int(seconds_to_days(time_difference))
+    credit_term = get_credit_days(customer_name)
+    if credit_term is None:
+         credit_term = 7
+    
+    outstandingdays = get_date_difference_from_last_sale_invoice(customer_name);
+    xx = int(credit_term) - int(outstandingdays) 
+
+    if xx is not None: 
         if xx < 0:  
             xx = abs(xx)  
             if user in om_profile:
@@ -75,95 +130,13 @@ def sales_order_on_submit(doc, method):
                 role = "ceo"
                  
         else:
-            exists = "approve"    
-        
+            exists = "approve" 
 
     if exists != 'approve':
-        converted_string = f'Credit Limit difference is {xx}. Contact upper-level permissions.'
+        converted_string = f'Credit Term difference is {xx}. Contact upper-level permissions. outstanding days is {outstandingdays} credit term days is {credit_term}'
         throw(converted_string)
     else:
         pass
-    
-    # customer_name = doc.customer
-    # customer = frappe.get_doc("Customer", doc.customer)
-    # user = frappe.get_doc("User", frappe.session.user)
-    # user = user.email
-
-
-    # posting_date = doc.transaction_date
-    # date_object = datetime.strptime(posting_date, "%Y-%m-%d")
-    # posting_date = datetime.timestamp(date_object)
-
-
-    # current_datetime = datetime.now()
-    # timestamp = datetime.timestamp(current_datetime)
-
-
-    # time_difference =  timestamp - posting_date
-    # days = int(seconds_to_days(time_difference))
-
-    # credit_term = get_credit_days(customer_name)
-    # if credit_term is None:
-    #     return
-
-    # doctype = "Credit Limit Settings"
-    # docz = frappe.get_doc(doctype, doctype)  
-    # om_profile = docz.om_profile
-    # ar_profile = docz.ar_profile
-    # ar_vp = docz.ar_vp
-    # ceo_profile = docz.ceo_profile
-    # outstandingdays = get_date_difference_from_last_sale_invoice(customer_name);
-    # credit_term_one = int(docz.credit_term_one)
-    # credit_term_two = int(docz.credit_term_two)
-    # credit_term_three = int(docz.credit_term_three)
-    # credit_term_four = int(docz.credit_term_four)
-
-    # xx = int(credit_term) - int(outstandingdays) 
-    # xx = abs(xx)
-
-
-    # if xx > credit_term_four:
-    #     approval_role = "CEO"
-    #     csv_values = ceo_profile
-    #     value_array = csv_values.split(",")
-    #     value_to_check = user
-    #     if value_to_check in value_array:
-    #         exists = "approve"
-    #     else:
-    #         exists = "Credit Term: Only CEO can approve"
-    
-    # elif xx > credit_term_three:
-    #     approval_role = "AR-VP"
-    #     csv_values = ar_vp
-    #     value_array = csv_values.split(",")
-    #     value_to_check = user
-    #     if value_to_check in value_array:
-    #         exists = "approve"
-    #     else:
-    #         exists = "Credit Term: Only AR-VP can approve"
-
-
-    # elif xx > credit_term_two:
-    #     approval_role = "Level 2"
-    #     csv_values = ar_profile
-    #     value_array = csv_values.split(",")
-    #     value_to_check = user
-    #     if value_to_check in value_array:
-    #         exists = "approve"
-    #     else:
-    #         exists = "Credit Term: Only AR Profile can approve"
-
-    # elif xx > credit_term_one:
-    #     approval_role = "Level 1"
-    #     csv_values = om_profile
-    #     value_array = csv_values.split(",")
-    #     value_to_check = user
-    #     if value_to_check in value_array:
-    #         exists = "approve"
-    #     else:
-    #         exists = "Credit Term: Only OM Profile can approve"
-    
-
     
           
     
@@ -190,16 +163,16 @@ def get_credit_days(customer_name):
 def get_date_difference_from_last_sale_invoice(customer_name):
     invoices = frappe.get_all(
         "Sales Invoice",
-        filters={"customer": customer_name, "docstatus": ["in", [1]]},
-        fields=["posting_date", "docstatus"],
-        order_by="posting_date desc",
-        limit_page_length=1 
+        filters={"customer": customer_name, "status": ["in", ['Overdue']]},
+        fields=["posting_date","due_date"]
     )
-
+    
+    diff = []
     if invoices:
-        last_invoice = invoices[0]
-        posting_date = last_invoice.posting_date
-        date_difference = datetime.now().date() - posting_date
-        return date_difference.days
-    else:
-        return 0
+        for item in invoices:
+            due_date = item['due_date']
+            date_difference = datetime.now().date() - due_date
+            diff.append(date_difference.days)
+        
+        maxz = max(diff)    
+        return maxz
