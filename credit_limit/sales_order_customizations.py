c5c1514e4a856efec9bd411b6837c0202b6a0002
Nabeel Goraya <itsgoraya@gmail.com>
2023-09-05 13:38:50 +0500

    Update sales_order_customizations.py

--- credit_limit/sales_order_customizations.py
+++ credit_limit/sales_order_customizations.py
@@ -138,8 +138,8 @@ def sales_order_on_submit(doc, method):
         else:
             exists = "approve" 
 
-converted_string = f'Credit Term difference is {xx}. Contact upper-level permissions. outstanding days is {outstandingdays} credit term days is {credit_term} {role}'
-throw(converted_string)
+        converted_string = f'Credit Term difference is {xx}. Contact upper-level permissions. outstanding days is {outstandingdays} credit term days is {credit_term} {role}'
+        throw(converted_string)
 
 
 def seconds_to_days(seconds):
