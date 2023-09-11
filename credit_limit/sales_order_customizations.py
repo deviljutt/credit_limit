e5d65d91bb678830bfba25a896492bf64442a74e
Nabeel Goraya <itsgoraya@gmail.com>
2023-09-11 13:20:40 +0500

    Update sales_order_customizations.py

--- credit_limit/sales_order_customizations.py
+++ credit_limit/sales_order_customizations.py
@@ -76,7 +76,7 @@ def sales_order_on_submit(doc, method):
 
         if exists != 'approve':
             msg = alert_message.replace('[diffrence]', xx)
-            converted_string = msg
+            converted_string = str(msg)
             throw(converted_string)
     
 
