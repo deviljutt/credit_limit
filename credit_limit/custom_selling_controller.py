2e95b77da10e2d032d340fda302b55c0a6b03211
Nabeel Goraya <itsgoraya@gmail.com>
2023-09-20 10:44:10 +0500

    Update custom_selling_controller.py

--- credit_limit/custom_selling_controller.py
+++ credit_limit/custom_selling_controller.py
@@ -1,10 +1,9 @@
-from erpnext.controllers.sales_and_purchase_return import SellingController
+from erpnext.controllers.selling_controller import SellingController
 from frappe.utils import flt
 
 def calculate_contribution(self):
     if not self.meta.get_field("sales_team"):
         return
 
-    
 # Assign the custom function to the SellingController class
 SellingController.calculate_contribution = calculate_contribution
