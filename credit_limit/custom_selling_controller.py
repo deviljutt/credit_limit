8f0422d3a8b366dc569f26aa0509a8cefe7a1947
Nabeel Goraya <itsgoraya@gmail.com>
2023-09-21 12:14:09 +0500

    Update custom_selling_controller.py

--- credit_limit/custom_selling_controller.py
+++ credit_limit/custom_selling_controller.py
@@ -2,8 +2,7 @@ from erpnext.controllers.selling_controller import SellingController
 from frappe.utils import flt
 
 
-    
-class updatesellingconrtoller(self):
+class updatesellingconrtoller():
     def calculatecontribution(self):
         if not self.meta.get_field("sales_team"):
             return
