c201fd83c030e58035bf904899e242ed1fc961ca
Nabeel Goraya <itsgoraya@gmail.com>
2023-09-21 15:15:43 +0500

    update

--- credit_limit/custom_selling_controller.py
+++ credit_limit/custom_selling_controller.py
@@ -3,6 +3,6 @@ from erpnext.controllers.stock_controller import StockController
 
 
 class updatesellingconrtoller(SellingController):
-    def calculatecontribution(self):
+    def calculate_contribution(self):
         if not self.meta.get_field("sales_team"):
             return
