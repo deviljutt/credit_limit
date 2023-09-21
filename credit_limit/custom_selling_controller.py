fd8d8553c818ec12c3d8be54c34b5ca2ff9f5c3f
Nabeel Goraya <itsgoraya@gmail.com>
2023-09-21 13:46:49 +0500

    update

--- credit_limit/custom_selling_controller.py
+++ credit_limit/custom_selling_controller.py
@@ -1,6 +1,4 @@
 from erpnext.controllers.selling_controller import SellingController
-from erpnext.controllers.stock_controller import StockController
-
 
 class updatesellingconrtoller(SellingController):
     def calculatecontribution(self):
