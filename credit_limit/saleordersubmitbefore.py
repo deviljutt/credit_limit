569d69ba3b45d131f58ca58b517c0a55cc057e25
Nabeel Goraya <itsgoraya@gmail.com>
2023-11-23 10:38:11 +0500

    Update saleordersubmitbefore.py

--- credit_limit/saleordersubmitbefore.py
+++ credit_limit/saleordersubmitbefore.py
@@ -5,9 +5,12 @@ from datetime import datetime
 
 
 def sales_order_on_submit(doc, method):
-    if doc.marketplace_order_number:
-        existing_order = frappe.db.exists({"doctype": "Sales Order", "marketplace_order_number": doc.marketplace_order_number})
-        if existing_order:
-            frappe.throw("Sales Order already exists for this marketplace order number: {}".format(doc.marketplace_order_number))
-        else:
-            pass
+    if hasattr(doc, 'marketplace_order_number') and doc.marketplace_order_number:
+        if doc.marketplace_order_number:
+            existing_order = frappe.db.exists({"doctype": "Sales Order", "marketplace_order_number": doc.marketplace_order_number})
+            if existing_order:
+                frappe.throw("Sales Order already exists for this marketplace order number: {}".format(doc.marketplace_order_number))
+            else:
+                pass
+    else:
+        pass
